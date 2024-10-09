a1e996cc9ac3dca299effda89277385e4ff7e20e
Gavin-Fetrow-Student <gafe134@student.oaisd.org>
2024-10-09 13:12:01 +0000

    Used a basic C# Program for first time

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("My name is Gavin Fetrow.");
+
+        //Using Variables
+        int age = 17;
+        string greeting = "Hello World";
+        string firstname = "Gavin";
+        string lastname = "Fetrow";
+        double money = 2.79;
+
+        Console.WriteLine(greeting + "! " + "My name is " + firstname + " " + lastname + ". I am " + age + " years old. I just spent $" + money + " on an Energy Drink.");
+    }
+
+}
